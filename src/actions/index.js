f29cb6f013fc8f3a719b22bf025d15f13cf1c23f
davidsugiarto <davidsugiarto)11@gmail.com>
2019-09-20 14:58:04 +0700

    changing todos

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,29 +4,6 @@ import { RSAA } from 'redux-api-middleware'
 import { push } from 'connected-react-router'
 import { search } from './pokemons'
 
-let nextTodoId = 0
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
-})
-
-export const setVisibilityFilter = filter => ({
-    type: 'SET_VISIBILITY_FILTER',
-    filter
-})
-
-export const toggleTodo = id => ({
-    type: 'TOGGLE_TODO',
-    id
-})
-
-export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_COMPLETED: 'SHOW_COMPLETED',
-    SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
-
 export const LOAD_POKE={
   REQUEST: 'LOAD_POKE',
   SUCCESS: 'LOAD_POKE_SUCCESS',
